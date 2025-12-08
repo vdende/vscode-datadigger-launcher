57f60459f2d8967feda10069ba9729e6b84b396e
Arno van der Ende <arno@vd-ende.com>
2025-12-08 20:17:20 +0100

    Put 'last used' on top in 'Start DataDigger' command

--- src/commands/startDataDiggerCommand.ts
+++ src/commands/startDataDiggerCommand.ts
@@ -25,15 +25,26 @@ export async function run(context: vscode.ExtensionContext): Promise<void> {
   if (ddProjects.size === 1) {
     const [ddProjectConfig] = ddProjects.values();
     await ddConfigs.startDataDigger(ddProjectConfig);
+    context.globalState.update("dd.lastProject", ddProjectConfig.projectName);
     return;
   }
 
-  // More projects -> show QuickPick
+  // More projects -> show QuickPick (sort by lastUsed)
+  const lastUsedProject = context.globalState.get<string>("dd.lastProject");
   const items: vscode.QuickPickItem[] = [];
   for (const [projectName, ddProjectConfig] of ddProjects.entries()) {
     items.push({
       label: projectName,
-      description: ddProjectConfig.projectDir ?? ddProjectConfig.dataDiggerPath ?? ''
+      description: ddProjectConfig.projectDir + (projectName === lastUsedProject ? " (last used)" : "")
+    });
+  }
+
+  // Sort: last used project first
+  if (lastUsedProject) {
+    items.sort((a, b) => {
+        if (a.label === lastUsedProject) return -1;
+        if (b.label === lastUsedProject) return 1;
+        return a.label.localeCompare(b.label); // alphabetic for the rest
     });
   }
 
@@ -51,6 +62,7 @@ export async function run(context: vscode.ExtensionContext): Promise<void> {
     vscode.window.showErrorMessage(`ABL DataDigger: configuration for project '${selection.label}' is not found!`);
     return;
   }
+  context.globalState.update("dd.lastProject", selection.label);
 
   await ddConfigs.startDataDigger(chosenConfig);
 }
